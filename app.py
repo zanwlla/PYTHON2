25739ca526fac16e67e8f5b29a7022099f34c155
Camila <zanellacamila016@gmail.com>
2024-04-12 12:05:07 -0300

    projeto em sala

--- app.py
+++ app.py
@@ -1,44 +1,61 @@
-#1 10/04 imoport lib os (operational system)
-import os
+import os 
 
+restaurante=[] 
 
-def chamar_nome_do_app():
-    print ("Restaurante Expresso\n")
-    
-def escolher_opcoes():
-    print ("1 Cadastrar Restaurante")
-    print ("2 Listar Restaurante")
-    print ("3 Ativar Restaurente")
-    print ("4 Sair\n")
-
-#2 declarando a função finalizar_app
 def finalizar_app():
     os.system('cls')
-    print ('Finalizando o app\n')
+    print ('Finalizando app\n')
 
-#8 declarando a função opcao_digitada1
-def opcao_digitada1():
-    opcao_digitada = (int(input("Escolha uma opção")))
-    print ("Você selecionou:",opcao_digitada, "\n")
-    if(opcao_digitada==1):
-        print("Você escolheu Cadastrar Restayrante\n")
-    elif(opcao_digitada==2):
-        print("Você escolheu Listar Restaurante\n") 
-    elif(opcao_digitada==3):
-         print ("Voce escolheu Ativar Restaurante\n")
-    #3 chamando a função finalizar_app
-    else:
-        finalizar_app()     
-  
-  #5 escrever a funçaõ main
+def chamar_name_do_app():
+     print ('Restaurante Expresso\n') 
+
+def opcao_invalida():
+    print('Opção invalida\n')
+    input('Digite uma tecla para voltar ao menu principal: ')
+    main() 
+
+def exibir_opcoes():
+    print('1-Cadastrar um restaurante')
+    print('2-Listar restaurantes')
+    print('3-Ativar um restaurante')
+    print('4-Sair do programa') 
+
+
+def cadastrar_novo_restaurante():
+     os.system('cls')
+     nome_do_restaurante=input('Digite o nome do novo restaurante: \n')
+     restaurante.append(nome_do_restaurante)
+     print(f'O restaurante {nome_do_restaurante}, foi cadastrado com sucesso\n')
+     input("Digite uma tecla para voltar ao menu principal: \n")
+     main()
+
+def escolher_opcoes(): 
+    try:
+        opcao_digitada=(int(input('Escolha uma opção: ')))
+        print('Você selecionou a opção: ',opcao_digitada,'\n')
+        if(opcao_digitada==1):
+            print('Você escolheu cadastrar um restaurante\n')
+            cadastrar_novo_restaurante()
+        elif (opcao_digitada==2):
+            print('Você escolheu listar os restaurantes do app\n')
+        elif (opcao_digitada==3):
+            print('Você escolheu ativar um restaurante\n')
+        elif (opcao_digitada==4):
+            print('Você escolheu sair do programa\n')
+            finalizar_app()
+        else:
+            opcao_invalida()
+    except:
+        opcao_invalida() 
+
 def main():
-    #6 chamar o nome do app
-    chamar_nome_do_app()
-    #7 chamar a escolha de opções
-    escolher_opcoes()
-    #9 chamar a opção digitada
-    opcao_digitada1()
+    os.system('cls') 
 
-#4 criando a entrada através da variável main
+    chamar_name_do_app() 
+
+    exibir_opcoes() 
+
+    escolher_opcoes() 
+
 if(__name__=='__main__'):
     main()
